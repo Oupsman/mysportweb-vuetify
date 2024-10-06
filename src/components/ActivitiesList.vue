ee0e54964f80aeb3bb7b1cae9233f3c20edf88d7
oupsman <oupsman@oupsman.fr>
2024-10-06 10:10:19 +0200

    Activity List : no more artefact on the left of the first activity

--- src/components/ActivitiesList.vue
+++ src/components/ActivitiesList.vue
@@ -3,6 +3,15 @@
   import ActivityCard from '@/components/ActivityCard.vue'
 
   const activitiesStore = useActivitiesStore()
+  const activitiesDisplay = ref([])
+  const pointer = ref(0)
+
+  async function load ({ done }) {
+    const newActivities = activitiesStore.activities.slice(pointer.value, pointer.value + 100)
+    pointer.value += 20
+    activitiesDisplay.value.push(...newActivities)
+    done('ok')
+  }
 
   onMounted(() => {
     activitiesStore.getActivities()
@@ -11,11 +20,11 @@
 </script>
 
 <template>
-  <v-container>
-    <v-sheet class="d-flex align-start flex-wrap mb-6 w-100 h-auto">
-      <ActivityCard v-for="activity in activitiesStore.activities" :key="activity.id" :activity="activity" />
-    </v-sheet>
-  </v-container>
+  <v-infinite-scroll :height="600" :items="activitiesDisplay" :onLoad="load">
+    <v-container class="d-flex align-start flex-wrap mb-6">
+      <ActivityCard v-for="activity in activitiesDisplay" :key="activity.id" :activity="activity" />
+    </v-container>
+  </v-infinite-scroll>
 </template>
 
 <style scoped lang="sass">
